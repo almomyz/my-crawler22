e35a99b242df724f911126c71430b80618f6c45b
Eng.Abdu khalid yahya Al_hrazi <82823586+almomyz@users.noreply.github.com>
2024-05-17 17:58:12 +0300

    fddddd

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
-// For more information, see https://crawlee.dev/
+import express from 'express';
 import { PuppeteerCrawler, ProxyConfiguration } from 'crawlee';
 import { router } from './routes.js';
 
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Define the start URLs and proxy configuration
 const startUrls = ['https://crawlee.dev'];
 
+
+
+// Create the PuppeteerCrawler instance
 const crawler = new PuppeteerCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
     // Comment this option to scrape the full website.
     maxRequestsPerCrawl: 20,
 });
 
-await crawler.run(startUrls);
+// Endpoint to start the crawler
+app.get('/start-crawl', async (req, res) => {
+    try {
+        await crawler.run(startUrls);
+        res.send('Crawling completed!');
+    } catch (error) {
+        console.error('Error during crawling:', error);
+        res.status(500).send('Error during crawling');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
